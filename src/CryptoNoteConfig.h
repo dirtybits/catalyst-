b9093d959e2bdf03b5cc9d1015696678055ccd51
andre <dirtybitsofficial@gmail.com>
2018-05-08 03:29:54 -0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -86,9 +86,9 @@ const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by def
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
 //TODO This port will be used by the daemon to establish connections with p2p network
-const int      P2P_DEFAULT_PORT                              = 17700;
+const int      P2P_DEFAULT_PORT                              = 13035; // 17700 potential alternative
 //TODO This port will be used by the daemon to interact with simplewallet
-const int      RPC_DEFAULT_PORT                              = 17701;
+const int      RPC_DEFAULT_PORT                              = 13036; // 17701 potential alternative
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
